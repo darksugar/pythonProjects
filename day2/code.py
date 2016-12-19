e85896d082e0bf339304ca29289f6fb96a4119d1
Darksugar <285898591@qq.com>
2016-12-19 21:13:48 +0800

    test

--- day2/code.py
+++ day2/code.py
@@ -2,7 +2,7 @@
 
 msg='天安门'
 
-print(msg.encode(encoding='gb2312'))
+print(msg.encode(encoding='GBK'))
 
 mss=b'\xcc\xec\xb0\xb2\xc3\xc5'
 
---
+++ day2/names.py
@@ -0,0 +1,15 @@
+#Author:Ivor
+
+names = ["zhangsan","lisi","wangwu"]
+
+names.append("lisirui")
+names.insert(1,"sss")
+names.remove("zhangsan")
+print(names)
+#del names[1]
+#names.pop()
+
+#print(names[0],names[2])
+#print(names[0:3])
+#print(names[-3:-1])
+#print(names[-1:])
