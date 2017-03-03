7f9788e8832f3a524310ede33a61f824b4526108
武伟 <285898591@qq.com>
2017-03-03 11:16:01 +0800

    commit

--- day6/shelve mod.py
+++ day6/shelve mod.py
@@ -1,1 +1,18 @@
 #Authon Ivor
+import shelve
+
+d = shelve.open('shelve_test')  # 打开一个文件
+
+class Test(object):
+    def __init__(self, n):
+        self.n = n
+
+t1 = Test("123")
+t2 = Test("ivor")
+name = ["alex","ivor"]
+
+d["name"] = name
+d["t1"] = t1
+d["t2"] = t2
+
+d.close()
