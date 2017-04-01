13d6283c629831ba43f9b1a5c4e3b7e3a7050499
武伟 <285898591@qq.com>
2017-04-01 16:48:30 +0800

    commit

--- day7/FTP/ftp_client.py
+++ day7/FTP/ftp_client.py
@@ -1,32 +1,35 @@
 #Authon Ivor
 import os
-import socket
 import json
+import socket
 
 client = socket.socket()
 client.connect(('localhost',9999))
-
 while True:
     choice = input(">>>:")
     if len(choice) == 0:continue
     cmd_list = choice.split()
     if cmd_list[0] == 'put':
-        if len(cmd_list) == 1:
-            print("Need a filename... ")
-            continue
-        filename = cmd_list[1]
-        if os.path.isfile(filename):
-            file_obj = open(filename,'rb')
-            base_filename = filename.split("/")[-1]
-            file_size = os.path.getsize(filename)
-            print(base_filename,file_size)
-            data_header = {
-                "action":"put",
-                "filename":base_filename,
-                "size":file_size
-            }
-            client.send( json.dumps(data_header).encode('utf-8') )
+        if cmd_list[-1]:
+            file_name = cmd_list[-1]
+            if os.path.isfile(file_name):
+                base_filename = file_name.split("/")[-1]
+                file_size = os.path.getsize(file_name)
+                file_obj = open(file_name,'rb')
+                data_header = {
+                    "action":"put",
+                    "file_name":base_filename,
+                    "file_size":file_size
+                }
+                client.send(json.dumps(data_header).encode('utf-8'))
+                for line in file_obj:
+                    client.send(line)
+                print("File send complete.".center(50,'-'))
+            else:
+                print("File is not exists.")
         else:
-            print("File is not exist.")
+            print("Need a file...")
     elif cmd_list[0] == 'get':
         pass
+    else:
+        print("Wrong input.")
--- day7/FTP/ftp_server.py
+++ day7/FTP/ftp_server.py
@@ -1,14 +1,31 @@
 #Authon Ivor
-
 import socket
+import json
+import os
 
+ip = ('localhost',9999)
 s = socket.socket()
-s.bind(('localhost',9999))
+s.bind(ip)
 s.listen(5)
 
 while True:
+    print("等待连接中....")
     conn,client_addr = s.accept()
-    print("Got a new conn",conn)
+    print("有用户主动接入:", conn, client_addr)
     while True:
-        data = conn.recv(1024)
-        print("recv data:",data)
+        data = conn.recv(4096)
+        data = json.loads(data.decode('utf-8'))
+        if data.get('action') is not None:
+            if data['action'] == 'put':
+                file_obj = open(data['file_name'],'wb')
+                receive_size = 0
+                file_size = data['file_size']
+                while receive_size < file_size:
+                    recv = conn.recv(4096)
+                    file_obj.write(recv)
+                    receive_size += len(recv)
+                    print(file_size,receive_size)
+                file_obj.close()
+                print("File receive complete!")
+            elif data['action'] == 'get':
+                pass
