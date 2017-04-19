d2014c278dff6b231af249572028c5eea7e72e20
武伟 <285898591@qq.com>
2017-04-19 13:33:27 +0800

    commit

--- day10/gevent mod 协程.py
+++ day10/gevent mod 协程.py
@@ -1,1 +1,16 @@
 #Authon Ivor
+import gevent
+
+def run():
+    print(1)
+    gevent.sleep(2)
+    print(6)
+def run2():
+    print(2)
+    gevent.sleep(1)
+    print(5)
+def run3():
+    print(3)
+    gevent.sleep(0)
+    print(4)
+gevent.joinall([gevent.spawn(run),gevent.spawn(run2),gevent.spawn(run3)])
