8af6ee1cc721b17fae8d60c6c7b919d8a5a5be4e
武伟 <285898591@qq.com>
2017-04-24 23:45:19 +0800

    commit

--- day10/Selector_FTP/FTP_Server/server.py
+++ day10/Selector_FTP/FTP_Server/server.py
@@ -2,7 +2,13 @@
 import socket
 import selectors
 import json
-
+import pickle
+import os
+STATUS_CODE = {
+    200: "Ready to receive file",
+    201: "Ready to send file",
+    301: "File not exists"
+    }
 class FTP_Server(object):
     def __init__(self):
         self.server = self.initialize()
@@ -22,30 +28,67 @@ class FTP_Server(object):
             event = self.select.select()
             for key, mask in event:
                 callback = key.data
-                callback(key.fileobj, mask)
+                callback(key.fileobj,mask)
 
-    def accept(self,sock,mask):
-        conn, addr = sock.accept()
+    def accept(self,conn,mask):
+        conn, addr = conn.accept()
         print("New connection",addr)
         conn.setblocking(False)
         self.select.register(conn,selectors.EVENT_READ,self.interactive)
 
     def interactive(self,conn,mask):
-        res = conn.recv(1024)
-        data = json.loads(res.decode())
+        res = conn.recv(4096)
+        # print("res",res)
+        data = pickle.loads(res)
+        # print("data,",type(data),data)
         if not data:conn.close()
         if data.get("action"):
             if hasattr(self, "_%s" % data.get("action")):
-                func = getattr(self,"_%s" % data.get("action"))
-                func(conn,data)
+                    func = getattr(self,"_%s" % data.get("action"))
+                    func(conn,data)
 
-    def _echo(self,conn,data):
-        conn.send(data.get("msg").encode())
+    def _put(self,conn,*args,**kwargs):
+        recv_data = args[0]
+        file_name = recv_data.get("file_name")
+        file_size = recv_data.get("file_size")
+        if os.path.isfile(file_name):
+            file_obj = open(file_name, 'ab')
+        else:
+            file_obj = open(file_name, 'wb')
+        if recv_data.get("detail") == "put_file":
+            conn.send(b"y")
+            recv = recv_data.get("line")
+            file_obj.write(recv)
+            if os.path.getsize(file_name) == file_size:
+                file_obj.close()
+                print("get done")
+        else:
+            self.send_response(conn,200)
+            print("send 200 done")
+            print(recv_data)
 
-    def _put(self):
-        pass
-    def _get(self):
-        pass
+    def _get(self,conn,*args,**kwargs):
+        recv = args[0]
+        file_name = recv.get("file_name")
+        if os.path.isfile(file_name):
+            file_size = os.path.getsize(file_name)
+            self.send_response(conn,201,{"file_size":file_size})
+            file_obj = open(file_name,"rb")
+            for data in file_obj:
+                conn.send(data)
+            else:
+                print("send done")
+        else:
+            self.send_response(conn,301)
+
+    def send_response(self,conn, status_code,data=None):
+        '''向客户端返回数据'''
+        response = {'status_code': status_code, 'status_msg': STATUS_CODE[status_code]}
+        if data:
+            response.update(data)
+        conn.send(json.dumps(response).encode('utf-8'))
+
+
 
 if __name__ == '__main__':
     server = FTP_Server()
